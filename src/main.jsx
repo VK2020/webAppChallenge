f7c1361605afc028cda60b54a9c46fc57ef47563
Vishwanth Koli <vishwanath.koli@sonata-software.com>
2020-07-25 15:25:30 +0530

    renamed

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import { Provider } from "react-redux";
 import store from "./store";
-import TabsComponent from './components/TabsComponent'
+import MainComponent from './components/MainComponent'
 
 ReactDOM.render(
     <Provider store={store}>
         <Router history={hashHistory}>
-            <Route path="/" component={TabsComponent}>
+            <Route path="/" component={MainComponent}>
             </Route>
         </Router>
     </Provider>,
